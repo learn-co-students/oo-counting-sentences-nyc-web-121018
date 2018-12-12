baa0a9a02aee300854c233ad6800dc42d0ceb72f
tfuquirk <timothyfquirk@gmail.com>
2018-12-11 21:04:50 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
